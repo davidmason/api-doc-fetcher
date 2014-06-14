ad1ede5d56a067bff3d228edd50ca5c0fef808ba
David Mason <dr.d.mason@gmail.com>
2014-06-14 12:55:26 +1000

    accept a command line argument
    The argument is the section name to look up from the doc page.

--- index.js
+++ index.js
@@ -1,31 +1,43 @@
 var https = require('https'),
     cheerio = require('cheerio')
 
+var section = process.argv[2];
 
-var url = "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/min"
+var sections = {
+  'summary': '#Summary',
+  'syntax': '#Syntax',
+  'desc': '#Description'
+};
 
-https.get(url, function (res) {
-  var data = '';
-  console.log('STATUS: ' + res.statusCode);
-  res.on('data', function(d) {
-    console.log('got data: ' + d.length);
-    data += d;
-  }).on('end', function () {
-    handleCompleteData(data);
-  });
-}).on('error', function(e) {
-  console.error(e);
-})
+if (!(section in sections)) {
+  console.error('invalid section: ' + section);
+} else {
+  var url = "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/min"
 
+  https.get(url, function (res) {
+    var data = '';
+    console.log('STATUS: ' + res.statusCode);
+    res.on('data', function(d) {
+      data += d;
+    }).on('end', function () {
+      handleCompleteData(data);
+    });
+  }).on('error', function(e) {
+    console.error(e);
+  })
 
-function handleCompleteData(data) {
-  console.log(data.length);
+  function handleCompleteData(data) {
+    console.log(data.length);
 
-  var $ = cheerio.load(data);
+    var $ = cheerio.load(data);
 
-  // get <p> after #Summary
-  $('#Summary').next().map(function (i, element) {
-    console.log($(element).text());
-  });
+    // get <p> after #Summary
+    $(sections[section]).next().map(function (i, element) {
+      console.log($(element).text());
+    });
 
+  }
 }
+
+
+
